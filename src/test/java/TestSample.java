d7bf934bfa3c4d8abebe251775217abfb5cda9f1
Karim <kndani@gmail.com>
2020-10-28 20:00:45 -0700

    Test 1

--- src/test/java/TestSample.java
+++ src/test/java/TestSample.java
@@ -1,3 +1,4 @@
+import org.testng.annotations.Optional;
 import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
 
@@ -6,14 +7,14 @@ public class TestSample {
 
     @Test
     @Parameters("temp")
-    public void test1(String temp) throws InterruptedException {
+    public void test1(@Optional String temp) throws InterruptedException {
         System.out.println(temp +i);
         i++;
     }
 
     @Parameters("temp")
     @Test
-    public void test2(String temp) throws InterruptedException {
+    public void test2(@Optional String temp) throws InterruptedException {
         System.out.println(temp +i);
         i++;
         int k=1/0;
